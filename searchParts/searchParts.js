a86baf9c35b4354ec32dcc98405e67d74d83ebac
GRIGORSARUKHANYAN <saruxanyan.grigor@gmail.com>
2025-01-12 15:32:38 +0400

    add delete function

--- searchParts/searchParts.js
+++ searchParts/searchParts.js
@@ -1,3 +1,120 @@
+function deletePartById(id) {
+    console.log("deletePartById(id)",id);
+    
+    fetch(`https://carparts-t0mi.onrender.com/api/carPart/${id}`, {
+        method: "Delete",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(() => {
+
+            alert("Պահեստամասը ջնջված է")
+let  updateDiv = document.getElementById('table-container');
+updateDiv.innerHTML=`
+    `
+
+        })
+        .catch((error) => {
+          console.error("Error ", error);
+          // Optionally, display an error message to the user
+        });
+}
+function reqUpdatePart(id) {
+    
+    let data ={
+        make:document.getElementById("updatemake").value.toLowerCase() ,
+        model : document.getElementById("updatemodel").value.toLowerCase(),
+        partName : document.getElementById("updatepartName").value.toLowerCase(),  
+        position : document.getElementById("updateposition").value.toLowerCase(),  
+        color : document.getElementById("updatecolor").value.toLowerCase(),  
+        isOriginal : document.getElementById("updateisOriginal").checked,  
+        location : document.getElementById("updatelocation").value.toLowerCase(),  
+        noth : document.getElementById("updatenoth").value.toLowerCase(),  
+        count : document.getElementById("updatecount").value,  
+        price : document.getElementById("updateprice").value,  
+    }  
+    console.log(data,id);
+    fetch(`https://carparts-t0mi.onrender.com/api/carPart/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+let  updateDiv = document.getElementById('table-container');
+
+updateDiv.innerHTML=``
+alert('պահեստամասը թարմացվեց')
+        })
+        .catch((error) => {
+          console.error("Error ", error);
+        });
+
+
+
+}
+function getPartById(id) {
+    console.log("getPartById");
+    
+    fetch(`https://carparts-t0mi.onrender.com/api/carPart/${id}`, {
+        method: "Get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+            data=data.result[0]
+            
+let  updateDiv = document.getElementById('table-container');
+console.log(data);
+
+updateDiv.innerHTML=`
+    <div id="updateDiv" style=" background-color: red; margin-left: 30%;"> 
+     
+  <input id="updatemake" placeholder="մակնիշ" value="${data.make}"><br>
+    <input id="updatemodel" placeholder="մոդել" value="${data.model}"><br>
+    <input id="updatepartName" placeholder="պահեստամասի անվան" value="${data.partName}"><br>
+    <input id="updateposition" placeholder="դիրք" value="${data.position}"><br>
+    <input id="updatecolor" placeholder="գույն" value="${data.color}"><br>
+
+        <div style="display: flex;">
+        <p>օրիգինալություն</p> <input type="checkbox" ${data.isOriginal ? "checked" : ""}  id="updateisOriginal"><br>
+        </div>
+    <input id="updatelocation"  placeholder="գտնվելու վայր" value="${data.location}"><br>
+    <textarea id="updatenoth" placeholder="նշումներ"  >${data.noth}</textarea><br>
+    <input id="updatecount" type="number"  placeholder="քանակ"value="${data.count}"><br>
+    <input id="updateprice" type="number"  placeholder="գին" value="${data.price}"><br>
+    <button style="border-radius: 20px; width: 120px; height: 50px; background-color: rgb(3, 158, 255);" onclick="reqUpdatePart('${data._id}')"> Թարմացնել</button>
+
+    </div>
+    `
+
+        })
+        .catch((error) => {
+          console.error("Error ", error);
+          // Optionally, display an error message to the user
+        });
+}
 
 function createTable(data) {
     const container = document.getElementById('table-container');
@@ -38,7 +155,8 @@ function createTable(data) {
             <td>${item.noth}</td>
             <td>${item.count}</td>
             <td>${item.price}</td>
-            <td><p>${item._id}</p></td>
+            <td> <i onclick="getPartById('${item._id}')" class="fa fa-pencil" style="font-size:24px"></i></td>
+            <td> <i onclick="deletePartById('${item._id}')" class="fa fa-trash-o" style="font-size:34px; color:red"></i></td>
         `;
         tbody.appendChild(row);
     });
